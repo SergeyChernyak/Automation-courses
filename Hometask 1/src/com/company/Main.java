6f3fbbb064e781e6d6ed7e1f7752ac69a7099120
Serj <Striker-Life@mail.ru>
2018-07-17 21:48:58 +0300

    refactoring task 1

--- Hometask 1/src/com/company/Main.java
+++ Hometask 1/src/com/company/Main.java
@@ -8,10 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        // Random random = new Random();
-        // int arr[] = new int[10];
-        // int method = 5;
-
         int method;
         boolean exit;
         exit = true;
@@ -36,20 +32,20 @@ public class Main {
                     case 2: {
                         System.out.print("Введите число для факториала: ");
                         int a = scan.nextInt();
-                        //int c = factorial(a);
                         System.out.println("Факториал числа " + a + " = " + factorial(a));
-                        System.out.println("=======================================");
+
                         break;
                     }
                     case 3: {
                         System.out.print("Введите количество элементов для ряда Фибоначчи: ");
                         int f1 = scan.nextInt();
-                        //   int arr2[] = fibonachi(f1);
                         System.out.println("Ряд Фибоначчи с " + f1 + " элементов" + ": " + Arrays.toString(fibonachi(f1)));
                         break;
                     }
                     case 4:
-                        System.out.println("Еще не реализован");
+                        System.out.print("Введите номер телефона: ");
+                        String str = scan.next();
+                        validator(str);
                         break;
                     case 5:
                         exit = false;
@@ -59,15 +55,12 @@ public class Main {
                         break;
                 }
         }
-
     }
-
 
     public static int[] init_massive (int el_mas){
         Random random = new Random();
         int mas[] = new int[el_mas];
         int i;
-
         for (i = 0; i < mas.length; i++) {
             mas[i] = random.nextInt(20);
         }
@@ -81,7 +74,6 @@ public class Main {
             sum = sum + mas[i];
         }
         average = sum / mas.length;
-       // System.out.println("Средее арифметическое" + ": " + average);
         return average;
     }
 
@@ -104,17 +96,76 @@ public class Main {
     }
 
 
- /*   public static void validator (String str) {
-        Scanner scanner = new Scanner(System.in);
-        String str1 = scanner.nextLine();
-        System.out.println(str1);
-        int dov_str1 = str1.length();
-        if (dov_str1 > 13) {
-            System.out.println("Не корректтный телефон");
+    public static int validator (String str) {
+        int sum = 0;
+        if ((str.length() > 13) || (str.length() < 10) || (str.matches("[\\D]+")))
+            System.out.println("Не корректный номер!");
+        else {
+            for (int i = 0; i < str.length(); i++) {
+                if (Character.isDigit(str.charAt(i))) {
+                    sum += Integer.parseInt(String.valueOf(str.charAt(i)));
+                }
+            }
+            System.out.println("Cумма чисел в строке" + " = " + sum);
+        }
+            ret_sum_str(sum);
+        return sum;
+    }
+
+    public static int ret_sum_str (int sum) {
+        int it_sum = 0;
+        String str_it_sum = "";
+        str_it_sum = Integer.toString(sum);
+        int j=1;
+        while (str_it_sum.length() > 1) {
+            it_sum = 0;
+            for (int i = 0; i < str_it_sum.length(); i++) {
+                it_sum += Integer.parseInt(String.valueOf(str_it_sum.charAt(i)));
+            }
+            str_it_sum = Integer.toString(it_sum);
+            System.out.println("Итерация " + j + " = " + str_it_sum);
+            j++;
+        }
+        System.out.println("Результат = " + ret_result(it_sum));
+        return it_sum;
+    }
+
+    public static String ret_result (int sum3) {
+        String res = "";
+        if (sum3 == 1){
+            res = "Один";
         }
         else
-
+        if (sum3 == 2) {
+            res = "Два";
+        }
+        else
+        if (sum3 == 3){
+            res = "Три";
+        }
+        else
+        if (sum3 == 4){
+            res = "Четыре";
+        }
+        else
+        if (sum3 == 5){
+            res = "Пять";
+        }
+        else
+        if (sum3 == 6){
+            res = "Шесть";
+        }
+        else
+        if (sum3 == 7){
+            res = "Семь";
+        }
+        else
+        if (sum3 == 8){
+            res = "Восемь";
+        }
+        else {
+            res = "Девять";
+        }
+        return res;
     }
-*/
-
 }
